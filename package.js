4d4e9ab8853a0d7b6b2aa21098cd5d641b708613
Reaper <botsitemanager@gmail.com>
2025-10-12 13:06:57 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+    "name": "snow-tiers-bot",
+    "version": "1.0.0",
+    "description": "Discord bot for Snow Tiers testing queue system",
+    "main": "index.js",
+    "scripts": {
+        "start": "node index.js"
+    },
+    "dependencies": {
+        "discord.js": "^14.14.1",
+        "dotenv": "^16.3.1"
+    },
+    "keywords": [
+        "discord",
+        "bot",
+        "queue",
+        "testing"
+    ],
+    "author": "Your Name",
+    "license": "MIT"
+}
